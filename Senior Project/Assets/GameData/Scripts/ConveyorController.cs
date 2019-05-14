9816a28d1a0d1c1d3aa74f884763dd74c037b348
nicholaicascio <lordcohliani@gmail.com>
2019-05-14 00:29:37 -0600

    random phrase generation
    I'm now randomly putting 'good' or 'bad' phrases at the start of the conveyor. Next I need to move them down the line

---
+++ Senior Project/Assets/GameData/Scripts/ConveyorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorController : MonoBehaviour
+{
+    float elapsed = 0f;
+    public float chanceOfBad = 5;
+    public float timeBetween = 1f;
+    public GameObject spawnPoint;
+    public GameObject badPhrase;
+    public GameObject goodPhrase;
+
+    private void Start()
+    {
+        //Vector3 spawn = new Vector3();
+        //spawn = spawnPoint.transform.position;
+        CreatePhrase();
+    }
+
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= timeBetween)
+        {
+            elapsed = elapsed % 1f;
+            //CreatePhrase();
+            MovePhrases();
+        }
+    }
+
+    void CreatePhrase()
+    {
+        int num = Random.Range(1, 10);
+        Debug.Log(num);
+        if (num <= 5)
+        {
+            Instantiate(badPhrase, spawnPoint.transform.position, Quaternion.identity);
+        }
+        if(num > 5)
+        {
+            Instantiate(goodPhrase, spawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+
+    void MovePhrases()
+    {
+        Debug.Log(Time.time);
+
+    }
+}
