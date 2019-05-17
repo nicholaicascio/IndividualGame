cac6fa69a0ee44dc2f0e4a34390f8ea0e72a0191
nicholaicascio <lordcohliani@gmail.com>
2019-05-17 10:37:34 -0600

    creating wave controller
    foundation for wave controller. it's going to spawn a stack of phrases that the conveyor controller will move down the line and eventually destroy. eventually I should make a game controller which will tell the wave cntroller how to create each wave

--- Senior Project/Assets/GameData/Scripts/WaveController.cs
+++ Senior Project/Assets/GameData/Scripts/WaveController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
+    //public int waveSize;
+    public int chanceOfBad;
+    public GameObject spawnPoint;
+    public GameObject badPhrase;
+    public GameObject goodPhrase;
+    public GameObject[] wave;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,33 @@ public class WaveController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void createWave(int size, int badChance)
+    {
+        chanceOfBad = badChance;
+        wave = new GameObject[size];
+        Debug.Log("this wave size is " + wave.Length.ToString());
+        for (int i = 0; i <= wave.Length; i++)
+        {
+            wave[i] = CreatePhrase();
+        }
+    }
+
+    GameObject CreatePhrase()
+    {
+        int num = Random.Range(1, 10);
+        if (num <= chanceOfBad)
+        {
+            return Instantiate(badPhrase, spawnPoint.transform.position, Quaternion.identity) as GameObject;
+        }
+        else if (num > chanceOfBad)
+        {
+            return Instantiate(goodPhrase, spawnPoint.transform.position, Quaternion.identity) as GameObject;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
