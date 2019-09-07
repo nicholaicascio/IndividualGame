83fc0ac0cc11c35e47759efe674de657c2b2997e
nicholaicascio <lordcohliani@gmail.com>
2019-09-06 22:38:47 -0600

    lighting and HUD
    added a light to make the projector screen look like it's reflecting light back. also added a hud so players can see wave and the errors so far

--- Senior Project/Assets/GameData/Scripts/WaveController.cs
+++ Senior Project/Assets/GameData/Scripts/WaveController.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveController : MonoBehaviour
 {
     public int currentWave;
     public int incorrectResponses;
     public int badMissed;
+
+    public Text ErrorsDisplay;
+    public Text WaveDisplay;
+
+    void Update()
+    {
+        ErrorsDisplay.text = (incorrectResponses + badMissed).ToString();
+        WaveDisplay.text = currentWave.ToString();
+    }
 }
